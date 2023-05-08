e0517ba1d54d31cd33ddb24d0c1a10fbbeb5b800
Lucas Galdino <lucasgaldino7@gmail.com>
2023-05-08 08:25:22 -0300

    dia 6

--- app-inicial/src/components/basicos/Circulo.tsx
+++ app-inicial/src/components/basicos/Circulo.tsx
@@ -1,17 +1,19 @@
 interface CirculoProps {
-    texto: string
-    quasePerfeito?: boolean
+  texto: string;
+  quasePerfeito?: boolean;
 }
 
 export default function Circulo(props: CirculoProps) {
-    return (
-        <div className={`
+  return (
+    <div
+      className={`
             flex justify-center items-center
             h-64 w-64 bg-cyan-500
             text-black font-black text-3xl
-            ${props.quasePerfeito ? 'rounded-2xl' : 'rounded-full'}
-        `}>
-            {props.texto}
-        </div>
-    )
+            ${props.quasePerfeito ? "rounded-2xl" : "rounded-full"}
+        `}
+    >
+      {props.texto}
+    </div>
+  );
 }
